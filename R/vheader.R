9a0b9f8a6cd5a161e74803cbf1dcb77be65a6971
Guillaume Filion <guillaume.filion@gmail.com>
2011-10-05 16:20:17 +0200

    Transformed vheader() into a formatting function. Not running. TODO: link the fields of vtag with the code of vheader.

--- R/vheader.R
+++ R/vheader.R
@@ -1,61 +1,32 @@
-vheader <- function(call) {
-# Produce a header with version tracking information.
-# Invoke as 'vheader(match.call())' within a function and
-# use output as header of output files.
+vheader <- function(x, comment.char="#", extrafields="") {
+# Return a vheader from the vtag attribute of 'x'.
+# The vheader is a character vector of length one, where
+# each line is prefixed by a comment character.
 
-  # Assert that the digest package is installed and loaded.
-  stopifnot(require (digest));
-
-  # Restore warnings upon exit (will turn them off later).
-  opt <- options();
-  on.exit(options(opt));
-
-  # Convert the call to characters. The called closure
-  # (function) is in 'args[1]', and the arguments in
-  # 'args[2]', 'args[3]', ...
-  args <- as.character(call)
-
-  # Get the package the function is taken from.
-  package <- environmentName(environment(get(args[1])));
-
-  # Gather base information.
-  info <- list(
-      "call" = deparse(call),
-      "platform" = paste(
-            Sys.info()[c("sysname", "nodename", "release")],
-            collapse = " "),
-      "directory" = getwd(),
-      "user" = Sys.info()[["login"]],
-      "date" = strftime(Sys.time()),
-      "R version" = paste(
-            R.version[c("major", "minor")],
-            collapse = "."
-      ),
-  );
-
-  # Turn of warnings in what follows.
-  options('warn' = -1);
-
-  try (
-    # Skip if called closure is not in a package.
-    info[[paste(package, "version")]] <-
-          packageDescription(package)[["Version"]],
-      silent = TRUE
-  );
-
-  for (arg in args) {
-     try (
-        # Add SHA1 digest if argument is named (otherwise skip).
-        info[[paste(arg,"SHA1")]] <- digest(get(arg), "sha1"),
-          silent = TRUE
-     );
-  }
-  
-  # Produce the header by pasting key/value pairs from 'info'.
-  return (paste(
-    sub("^", "# ", names(info)),
-    sub("$", "\n", info),
-    sep = ": ",
-    collapse = ""
-  ));
+   if (is.null(attr(x, "vtag"))) {
+      # If x has no vtag, return NULL.
+      return(NULL);
+   }
+   
+   # The header should contain the fields from "session",
+   # "call", "self SHA1", and 'extrafields' if present.
+   if (comment.char != "") {
+      # Append a space after comment character (esthetics).
+      comment.char <- paste(comment.char, " ", sep="");
+   }
+   pairlist <- attr(x, "vtag")[[c(
+         "session",
+         "call",
+         "self SHA1",
+         extrafields)]];
+   return (paste(collapse="",
+      sapply(pairlist, FUN = function(pairs) {
+         # Concatenate key/value pairs.
+         paste(
+            sub("^", comment.char, names(pairs)),
+            sub("$", "\n", pairs),
+            sep=": ",
+            collapse="");
+         })
+   ));
 }
