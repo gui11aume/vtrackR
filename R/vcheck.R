7a42706df492bc4682823333f7f86869d1e1f2d1
Guillaume Filion <guillaume.filion@gmail.com>
2011-10-05 02:04:47 +0200

    Running. Changed the call to vtag. Replaced 'object SHA1' by 'self SHA1'.

--- R/vcheck.R
+++ R/vcheck.R
@@ -1,31 +1,28 @@
 vcheck <- function(X) {
-# Check the SHA1 digest of 'X' against its stored vtag attribute,
-# if present.
+# Check the SHA1 digest of 'X' against its vtag attribute.
 # Returns TRUE if the same, FALSE otherwise.
 # We may want to give a warning/error as well.
 
-  # Assert that the digest package is installed and loaded.
-  stopifnot(require (digest));
+   # Assert that the digest package is installed and loaded.
+   stopifnot(require (digest));
 
-  # Restore warnings upon exit (will turn them off later).
-  opt <- options();
-  on.exit(options(opt));
+   # Restore warnings upon exit (will turn them off later).
+   opt <- options();
+   on.exit(options(opt));
 
-  # Check vtag.
-  if (!is.null(X)) {
-     if (is.null(attributes(X)$vtag) ||
-         is.null(attributes(X)$vtag[["object SHA1"]])) {
-        warning("Can't check: no vtag exists for this object!");
-     } else {
-        vtag_sha1 <- attr(X, "vtag")[["object SHA1"]];
-        attr(X, "vtag") <- NULL;
-        obj_sha1 <- digest(X, "sha1");
-        return(identical(vtag_sha1, obj_sha1));
-     }
-  }
+   # Check vtag.
+   if (is.null(attr(X, 'vtag')) ||
+         is.null(attr(X, 'vtag')[["self SHA1"]])) {
+      warning("object has no vtag or no SHA1 digest");
+   }
+   else {
+      sha1_vtag <- attr(X, "vtag")[["self SHA1"]];
+      # Erase vtag, as it modifies the SHA1 digest.
+      attr(X, "vtag") <- NULL;
+      sha1_X <- digest(X, "sha1");
+      return(identical(sha1_vtag, sha1_X));
+   }
 
   return(FALSE);
 
 }
-
-
--- R/vtag.R
+++ R/vtag.R
@@ -1,34 +1,29 @@
 vtag <- function(...) {
-# Produce a vtag with version tracking information and add as
-# attribute 'vtag'.
-# Invoke as 'vtag(obj)' within a function and use output as 
-# new instance of 'obj', including a (new) 'vtag' attribute.
+# Add a 'vtag' attribute to an object. In function definitions,
+# replace 'return(x)' by 'return(vtag(X=x))'.
 
-  # Assert that the digest package is installed and loaded.
-  stopifnot(require (digest));
+   # Assert that the digest package is loaded.
+   stopifnot(require (digest));
 
-  # Restore warnings upon exit (will turn them off later).
-  opt <- options();
-  on.exit(options(opt));
+   # Technical section to emulate the prototype 'vtag(X)'.
+   if (length(list(...)) != 1) {
+      stop('vtag takes exactly one argument');
+   }
+   argname <- names(list(...));
+   if (is.null(argname) || argname == "X") {
+      X <- ..1;
+   }
+   else {
+      stop('argument "X" is missing, with no default');
+   }
 
-  # Object to be tagged.
-  X <- list(...)$X;
+   # Restore warnings upon exit (will turn them off later).
+   opt <- options();
+   on.exit(options(opt));
 
-  # Retrieve the parent call.
-  # TODO: check wht happens if it doesn't exist.
-  parent_call <- match.call(call=sys.call(sys.parent()));
-
-  # Convert the parent call to characters. The called closure
-  # (function) is in 'parent_args[1]', and the arguments in
-  # 'parent_args[2]', 'parent_args[3]', ...
-  parent_args <- as.character(parent_call);
-
-  # Get the package the function is taken from.
-  package <- environmentName(environment(get(parent_args[1],
-      pos=parent.frame())));
-
-  # Gather base information.
-  info <- list(
+   
+   # Gather environment information.
+   info <- list(
       "platform" = paste(
             Sys.info()[c("sysname", "nodename", "release")],
             collapse = " "),
@@ -39,51 +34,62 @@ vtag <- function(...) {
             R.version[c("major", "minor")],
             collapse = "."
       )
-  );
+   );
 
+   # Retrieve the parent call, ie the call to the function
+   # where 'vtag()' is embedded.
+   # TODO: check wht happens if it doesn't exist.
+   parent_call <- match.call(call=sys.call(sys.parent()));
+   info[["call"]] <- deparse(parent_call);
 
-  # Save parent call to function.
-  info[["call"]] <- deparse(parent_call);
+   # Convert the parent call to characters. The parent
+   # function is in 'parent_args[1]', and its arguments in
+   # 'parent_args[2]', 'parent_args[3]', ...
+   parent_args <- as.character(parent_call);
 
-  ## Put SHA1 value of arguments in sub-list to avoid name collissions.
-  info[["args"]] <- list();
+   # Get the 'package' of the parent (can be .GlobalEnv).
+   package <- environmentName(environment(get(parent_args[1],
+      pos=parent.frame())));
 
-  # Turn of warnings in what follows.
-  options('warn' = -1);
+   # Put SHA1 of arguments in sub-list to avoid name collissions.
+   info[["args"]] <- list();
 
-  for (arg in parent_args) {
-     # Try to retrieve object FROM PARENT-OF-PARENT ENVIRONMENT. 
-     # Will (silently) fail if not properly named.
-     arg_obj <- NULL;
-     try(arg_obj <- get(arg, envir=parent.frame(n=2)), silent=TRUE);
-     
+   # Turn off warnings in what follows.
+   options('warn' = -1);
 
-     # Add SHA1 digest if argument is named (otherwise skip).
-     if (!is.null(arg_obj)) {
-     # Clear vtag before calculating SHA1 digest, if it exists.
-        if (!is.null(attributes(arg_obj)$vtag)) {
+   for (arg in parent_args) {
+      # Try to retrieve object FROM PARENT-OF-PARENT ENVIRONMENT. 
+      # Will (silently) fail if not properly named.
+      arg_obj <- NULL;
+      try(
+          arg_obj <- get(arg, envir=parent.frame(n=2)),
+          silent = TRUE
+      );
+
+      # Add SHA1 digest if argument is named (otherwise skip).
+      if (!is.null(arg_obj)) {
+      # Clear vtag before calculating SHA1 digest, if it exists.
+         if (!is.null(attributes(arg_obj)$vtag)) {
             attr(arg_obj, "vtag") <- NULL;
-        }
-        # Calculate and add SHA1 digest.
-        info$args[[paste(arg,"SHA1")]] <- digest(arg_obj, "sha1");
-     }
-  }
+         }
+         # Calculate and add SHA1 digest.
+         info$args[[paste(arg,"SHA1")]] <- digest(arg_obj, "sha1");
+      }
+   }
 
-  try (
-    # Skip if called closure is not in a package.
-    info[[paste(package, "version")]] <-
+   try (
+      # Skip if called closure is not in a package.
+      info[[paste(package, "version")]] <-
           packageDescription(package)[["Version"]],
       silent = TRUE
-  );
+   );
 
-  # Add info as attribute (including SHA1 value of object itself).
-  if (!is.null(X)) {
-     if (!is.null(attributes(X)$vtag)) attr(X, "vtag") <- NULL;
-     info[["object SHA1"]] <- digest(X, "sha1");
-     attr(X, "vtag") <- info;
-  }
+   # Add info as attribute (including SHA1 of X itself).
+   if (!is.null(attributes(X)$vtag)) attr(X, "vtag") <- NULL;
+   info[["self SHA1"]] <- digest(X, "sha1");
+   attr(X, "vtag") <- info;
 
-  invisible(X);
+   return(X)
 
 }
 
